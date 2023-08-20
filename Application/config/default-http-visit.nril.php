ec7af84f9552b3d35cee95427f9e7ee039cb48ea
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-20 17:51:54 +0300

    Delete default-http-visit.nril.php

--- Application/config/default-http-visit.nril.php
+++
@@ -1,7 +0,0 @@
-<?php return Route::assign(
-  include 'default-host.nril.php',
-  include 'default-remote-host.nril.php',
-  include 'default-protocol.nril.php',
-  include 'default-http-header.nril.php',
-  include 'default-http-get.nril.php'
-); //<?>static call returns new Route [->assign], ...]&Route::merge=>new Router
