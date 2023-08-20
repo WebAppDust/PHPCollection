a3025a789a8e4217fb4d6ed7438223ce2d14b9b5
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-20 17:39:54 +0300

    Update default-http-visit.preset.nril.php

--- Application/config/Routes/default-http-visit.preset.nril.php
+++ Application/config/Routes/default-http-visit.preset.nril.php
@@ -1,10 +1,10 @@
 <?php return Collection\Patterns\Route::assign( //Note: NativeRIL is struct{type=>list...
   include (dirname(__FILE__).DS.'default-host.preset.nril.php'),
-  include 'default-executor.preset.nril.php'),                                             
-  include 'default-remote-host.preset.nril.php'),
-  include 'default-connection.preset.nril.php'),                                             
-  include 'default-protocol.preset.nril.php'),
-  include 'default-http-header.preset.nril.php'),
-  include 'default-http-path.preset.nril.php'),
+  include (dirname(__FILE__).DS.'default-executor.preset.nril.php'),                                             
+  include (dirname(__FILE__).DS.'default-remote-host.preset.nril.php'),
+  include (dirname(__FILE__).DS.'default-connection.preset.nril.php'),                                             
+  include (dirname(__FILE__).DS.'default-protocol.preset.nril.php'),
+  include (dirname(__FILE__).DS.'default-http-header.preset.nril.php'),
+  include (dirname(__FILE__).DS.'default-http-path.preset.nril.php'),
   [ 'type'=>"query" ] //empty
 ); //<?>static call builder method returns new Route [->assign], ...]&Route::merge=>new Router
