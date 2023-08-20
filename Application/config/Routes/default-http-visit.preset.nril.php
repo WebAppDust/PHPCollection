8eccb7a9ae73dfb347cc101bcfd38145d4bf2dd6
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-20 17:16:52 +0300

    Create default-http-visit.preset.nril.php

---
+++ Application/config/Routes/default-http-visit.preset.nril.php
@@ -0,0 +1,10 @@
+<?php return Collection\Patterns\Route::assign( //Note: NativeRIL is struct{type=>list...
+  include 'default-host.preset.nril.php',
+  include 'default-executor.preset.nril.php',                                             
+  include 'default-remote-host.preset.nril.php',
+  include 'default-connection.preset.nril.php',                                             
+  include 'default-protocol.preset.nril.php',
+  include 'default-http-header.preset.nril.php',
+  include 'default-http-path.preset.nril.php',
+  [ 'type'=>"query" ] //empty
+); //<?>static call builder method returns new Route [->assign], ...]&Route::merge=>new Router
