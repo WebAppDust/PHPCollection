03da4f164ab4287b56939ea3d2c53cc264fae971
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-20 16:32:13 +0300

    Create default-executor.preset.nril.php

---
+++ Application/config/Routes/default-executor.preset.nril.php
@@ -0,0 +1,5 @@
+<?php return [
+  'type'=>'executor', //required
+  'executor' => 'server',
+  'port' => '8080', // * for executable source him controller is a host machine task or another request
+];
