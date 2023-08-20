3c1471c46e13921e5f96062c8a82992143315a94
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-20 16:20:26 +0300

    Create default-host.preset.nril.php

---
+++ Application/config/Routes/default-host.preset.nril.php
@@ -0,0 +1,7 @@
+<?php return [
+  'type'=>'host', //required
+  'MAC' =>'*',
+  'name' => ['localhost', 'mysite'],
+  'addr' => ['127.0.0.1'],
+  'port' => [ 80, 8080 ]
+]; // continually 'behaviour' of route as executor
