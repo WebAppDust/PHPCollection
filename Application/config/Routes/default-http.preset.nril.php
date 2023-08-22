f67f814e9698a50b349e349c3f1d4459f1175eb6
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-22 03:15:16 +0300

    Update default-http.preset.nril.php

--- Application/config/Routes/default-http.preset.nril.php
+++ Application/config/Routes/default-http.preset.nril.php
@@ -1,8 +1,9 @@
 <?php return Collection\Patterns\Route::assign(
-  include (dirname(__FILE__).DS.'default-host.preset.nril.php'),
-  include (dirname(__FILE__).DS.'default-executor.preset.nril.php'),                                             
-  include (dirname(__FILE__).DS.'default-remote-host.preset.nril.php'),
-  include (dirname(__FILE__).DS.'default-connection.preset.nril.php'),                                             
-  include (dirname(__FILE__).DS.'default-protocol.preset.nril.php')
+  include (dirname(__FILE__)._DS.'default-host.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-executor.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-arguments.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-remote-host.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-connection.preset.nril.php'),                                             
+  include (dirname(__FILE__)._DS.'default-protocol.preset.nril.php')
   /* ... */
 );
