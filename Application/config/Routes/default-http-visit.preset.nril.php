5016dc3a77cde7e1d22cb1a73c47be853d2eed44
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-22 03:12:23 +0300

    Update default-http-visit.preset.nril.php

--- Application/config/Routes/default-http-visit.preset.nril.php
+++ Application/config/Routes/default-http-visit.preset.nril.php
@@ -1,11 +1,12 @@
 <?php return Collection\Patterns\Route::assign( //Note: NativeRIL is struct{type=>list...
-  include (dirname(__FILE__).DS.'default-host.preset.nril.php'),
-  include (dirname(__FILE__).DS.'default-executor.preset.nril.php'),                                             
-  include (dirname(__FILE__).DS.'default-remote-host.preset.nril.php'),
-  include (dirname(__FILE__).DS.'default-connection.preset.nril.php'),                                             
-  include (dirname(__FILE__).DS.'default-protocol.preset.nril.php'),
-  include (dirname(__FILE__).DS.'default-http-header.preset.nril.php'),
-  include (dirname(__FILE__).DS.'default-http-path.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-host.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-executor.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-arguments.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-remote-host.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-connection.preset.nril.php'),                                             
+  include (dirname(__FILE__)._DS.'default-protocol.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-http-header.preset.nril.php'),
+  include (dirname(__FILE__)._DS.'default-http-path.preset.nril.php'),
   [ 'type'=>"query" ] //empty
 ); //<?>static call builder method returns new Route [->assign], ...],
    //the incomplete object, where Route[r]::merge=>new Router
