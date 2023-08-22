b3e908c3cc0eb98ed46bc8d33da18542b326af59
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-23 01:13:46 +0300

    Update default-http-header.preset.nril.php

--- Application/config/Routes/default-http-header.preset.nril.php
+++ Application/config/Routes/default-http-header.preset.nril.php
@@ -1,6 +1,12 @@
 <?php return [
   'type'=>'header',
+  'method'=>"get",
   'User-Agent'=>'*',
-  'Connection'=>'',
-  'method'    =>"get"
+  'Accept'=>['include'=>'text/*'],
+  'Accept-Language'=>'en-US,en;q=0.5',
+  'Accept-Encoding'=>'gzip, deflate, br',
+  'Referer'=>'*',
+  'Connection'=>'keep-alive',
+  'Cache-Control'=>'max-age=0',
+  'Upgrade-Insecure-Requests'=>1,
 ];
