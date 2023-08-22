04d1afe7f49a4fea374b8bc7b986381790ac33d8
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-23 02:27:17 +0300

    Update default-executor.preset.nril.php

--- Application/config/Routes/default-executor.preset.nril.php
+++ Application/config/Routes/default-executor.preset.nril.php
@@ -1,4 +1,5 @@
 <?php return [
   'type'=>'executor', //required
   'executor' => 'server' // * for executable source him controller is a host machine task or another request
+                        //['php*', 'apache*']
 ];
