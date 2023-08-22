718ed8f566aaf3f3489d6af596674a760c1112fa
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-22 03:33:31 +0300

    Create Environment.php

---
+++ Collection/Environment.php
@@ -0,0 +1,11 @@
+<?php
+namespace Collection;
+/** Interpreter(depended) Environment Reader Interface.
+* It's a finnally `static` route.
+* Intrptr always has storing and sharing received arguments.
+* By any specific I-face. Storing inside script is faster, but
+* insecure way(instead of wrappers calling).
+**/
+class Environment{
+    
+}
