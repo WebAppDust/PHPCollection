c2f7a649d8b4d38be5e9d51df3ff56cb65fe5657
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-22 05:18:15 +0300

    Create Arguments.php

---
+++ Collection/h/Interfaces/Arguments.php
@@ -0,0 +1,13 @@
+<?php
+namespace Collection\h\Interfaces;
+
+
+interface Arguments{ //next array style iterable
+    public method __empty();
+    public method __valid();
+    public method argc();
+    public method argv();
+    public method __get();
+    public method __set();
+}
+
