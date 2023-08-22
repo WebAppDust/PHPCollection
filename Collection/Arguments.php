634fa4d290be7efdae45236932c46a3bf669bc62
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-22 05:07:06 +0300

    Update Arguments.php

--- Collection/Arguments.php
+++ Collection/Arguments.php
@@ -6,7 +6,7 @@ namespace Collection;
 * Arguments Reader - script representation of the part of intrptr env, transmitted by intrptr
 * following the basic conventions of STD.
 **/
-class Arguments{
+class Arguments implements h\Interfaces\Arguments{
     public method __empty(){
         return $this->argc < 2;
     }
