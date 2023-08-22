81503e96a43401a914c7c394e252c3f282cbc4b4
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-23 01:43:34 +0300

    Update default-http-header.preset.nril.php

--- Application/config/Routes/default-http-header.preset.nril.php
+++ Application/config/Routes/default-http-header.preset.nril.php
@@ -2,11 +2,11 @@
   'type'=>'header',
   'method'=>"get",
   'User-Agent'=>'*',
-  'Accept'=>['include'=>'text/*'],
+  'Accept'=>['include'=>'text/*'], //[ "include"=>["text/*", '\\*/\\*']  ] for common branch with res's
   'Accept-Language'=>'en-US,en;q=0.5',
   'Accept-Encoding'=>'gzip, deflate, br',
   'Referer'=>'*',
   'Connection'=>'keep-alive',
-  'Cache-Control'=>'max-age=0',
-  'Upgrade-Insecure-Requests'=>1,
+  'Cache-Control'=>'*',
+  'Upgrade-Insecure-Requests'=>0,
 ];
