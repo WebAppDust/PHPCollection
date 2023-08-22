cff4b9a1c140acc50df824bbde9a2186173bea26
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-22 04:59:35 +0300

    Create Arguments.php

---
+++ Collection/Arguments.php
@@ -0,0 +1,15 @@
+<?php
+namespace Collection;
+
+
+/**
+* Arguments Reader - script representation of the part of intrptr env, transmitted by intrptr
+* following the basic conventions of STD.
+**/
+class Arguments{
+  
+}
+
+
+class ArgumentsIterator{}
+
