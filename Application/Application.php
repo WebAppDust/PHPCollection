551bcdf2e2b81389bde69fa5ad6f14f58d15e939
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-22 06:08:22 +0300

    Update Application.php

--- Application/Application.php
+++ Application/Application.php
@@ -5,7 +5,7 @@
 //namespace Application;
 defined( '_CD' )? define( '_CD', dirname( __FILE__ ) ): null;
 defined( '_CF' )? define( '_CF', basename( __FILE__ ) ): null;
-include_once _CD.DIRECTORY_SEPARATOR.'defines.php';
+include_once _CD.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'defines.php';
 
 $router = Collection\Patterns\Router::getInstance();
 $router::assign( new Route([], function(){ return 0; }) );
