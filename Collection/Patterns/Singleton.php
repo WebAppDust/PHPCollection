bd5d6cb7db274ca77d335b8d156c94f142d32a89
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-24 06:41:36 +0300

    Update Singleton.php

--- Collection/Patterns/Singleton.php
+++ Collection/Patterns/Singleton.php
@@ -23,3 +23,47 @@ final class Singleton { //
     
     public static function isValidSingleton( $class ){}
 }
+
+
+function runtest($cls){
+    $class = null;
+    try{
+        $class = Singleton::getInstance();
+    }catch( \Throwable $e ){}
+    
+    if( $class != null ) throw new \Exception("Methods for childs included into protoType Singleton");
+    
+    $class = null;
+    try{
+        $class = new Singleton;
+    }catch( \Throwable $e ){}
+    
+    if( $class != null ) throw new \Exception("Constructor of Prototype(S) is callable");
+    
+    if ( Singleton::isSingleton( $cls ) ){
+        if( !Singleton::isInstantd( $cls ) ){
+            
+            if(false){
+                throw new \Exception("Methods for prototype included into object Singleton");
+            }
+
+            $class = $cls::getInstance();$class->pp=1;
+            $a=print_r($class,1);
+            $class->__destruct(); unset($class);
+            $b=print_r($cls::getInstance(),1);
+            if ( !$a == $b ){
+                throw new \Exception("object reconstructed");
+            }
+            
+            
+        }else{ throw new \Exception("test isInstantinated is true"); }
+    }else{ throw new \Exception("test isSingleton is false"); }
+}
+
+class An extends h\Singleton{ 
+    private static $__instance=null; 
+}
+runtest( "\Collection\Patterns\An" );
+
+
+
