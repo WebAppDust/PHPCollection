110bc25b5ad9087f0cf7ad219e0ecd4f5547400f
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-24 05:33:46 +0300

    Update and rename MemberException.php to CommonException.php

---
+++ Collection/Exceptions/CommonException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Behaviour of Application Exception.
+ **/
+namespace Collection\Exceptions;
+
+
+class CommonException extends Exception{
+    function _construct( $msg, $code, $previous ){
+        
+    }
+}
