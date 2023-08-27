4af568e1b955b9e7c3962ce95cdcae92e928ddeb
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-08-27 08:30:06 +0300

    Update Singleton.php

--- Collection/Patterns/h/Singleton.php
+++ Collection/Patterns/h/Singleton.php
@@ -1,41 +1,39 @@
 <?php
 /**
- * 
+ * Singleton abstraction layer.
  **/
 namespace Collection\Patterns\h; 
 
 
-abstract class Singleton implements \h\Interfaces\Singleton{
-    protected private /*const*/$__instance;
+final abstract class Singleton implements \h\Interfaces\Singleton{
+    final protected static /* private const*/ $__instance = null;
     
     
-    protected static function getInstance() {
-        if ( !self::$__instance ){
+    final protected static function getInstance() {
+        if ( self::$__instance === null ){
             self::$__instance = new /*self*/ static;
         }
         return self::$__instance;
     }
 
     
-    public final static function Instance(){
+    final public static function Instance(){
         throw new Collection\Exceptions\SingletoneException(
             Collection\Exceptions\SingletoneException::RFC001
         );
     }
-
-    public static function hasInstance(){
+    
+    final public static function hasInstance(){
         return null !== self::$__instance;
     }
-    
-    function __construct(){
+     
+    protected function __construct(){
         if ( self::$__instance ){
             throw new Collection\Exceptions\SingletoneException(
                  Collection\Exceptions\SingletoneException::RFC000
             );
         }
     }
-
-    final public function __destruct(){ /*not destroy*/ }
 }
 
 
