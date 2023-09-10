d0ec53f4055708a38d676f9791ef19536e2a7067
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2023-09-10 16:17:27 +0300

    Update environment_70033.php

--- Collection/drv/PHP/environment_70033.php
+++ Collection/drv/PHP/environment_70033.php
@@ -1,5 +1,21 @@
 <?php
-//language specification self-description (just a li'l bit)
+//PHP-70033 Language specification self-description (just a li'l bit)
 class environment_70033{
-  
+   function getExecutor(){
+     return [
+       "name": "phpstream",
+       "type"=>"stream",
+       "scheme"=>'Collection/schm/PHP/spec_'.PHP_VERSION_ID.'.php'
+     ];
+   }
+   
+   function getExecutionStack(){
+     return [
+       "type"=>"interpreter",
+       "name"=>basename(PHP_BINARY),
+       "location"=>dirname(PHP_BINARY),
+       "scheme"=>'path/to/schm/PHP/'.basename(PHP_BINARY).DIRECTORY_SEPARATOR.PHP_VERSION_ID.'.php'
+       "executor"=>&$this->executor
+     ];
+   }
 }
