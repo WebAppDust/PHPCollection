5d031c51c11d39373c6e687777c63be092acdec0
Serguchev Roman 95 <134163419+WebAppDust@users.noreply.github.com>
2024-03-15 20:18:01 +0300

    Update redefinitions.php

--- Collection/redefinitions.php
+++ Collection/redefinitions.php
@@ -4,4 +4,9 @@
 define( '_DS', DIRECTORY_SEPARATOR );
 define( '_FD', dirname( __FILE__ ) );
 
-define( '_API', 0 ); //branch commutation
+define( '_API', _FD.PHP_VERSION_ID._DS ); //branch commutation
+if( defined("PHP_VERSION_ID") == null || PHP_VERSION_ID < 70000 || PHP_VERSION_ID > 70035 || !is_dir(_API) ){
+    die("FatalError: incompatible environment(".print_r(@PHP_VERSION_ID)."), execution stopped in ".__FILE__); exit();
+    //note: however, the namespace has already been used.
+}
+
